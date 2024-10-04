c305b5ebb4d3bac6f4ca8d50531627dcea0d8500
dianaCiganda <dianaciganda@gmail.com>
2024-10-03 22:08:37 -0300

    se pide entrada de texto

--- entrega_3.ts
+++ entrega_3.ts
@@ -2,10 +2,10 @@
 // function isTextInPara(fullText: string, searchTerm: string): boolean {
   //   return fullText.includes(searchTerm);
   // }
-  
+  import * as rs from "readline-sync";
   const paragraph: string =
     "Urania. Qué nombre. A la gente le gusta, le parece sonoro, original, raro. A ella también, aunque le evoca sólo cosas tristes. Todo le evoca cosas tristes, ahora que ha vuelto. Pero ¿cuándo no, desde que se fue de esta ciudad, a la que creía no volvería nunca? ¿Y cómo era posible que alguien se llamara así en una familia de dominicanos católicos, que adoraban a la Virgen del Rosario y a la Virgen de Altagracia? Su padre le contó que su madre era quien escogió su nombre. Urania: la Musa de la astronomía. Nadie se atrevió a contradecirla. A él le parecía bien, se lo dijo como si la elección del nombre hubiera sido cosa de poca importancia. En aquellos días no había problemas en la casa. Las cosas cambiaron después, claro. Cambiaron tanto.";
-  const palabraBuscada:string="cosas";
+  const palabraBuscada:string=rs.question("ingrese la palabra a buscar: ");
  
 //completar la funcion para que devuelva la cantidad de veces que searchTerm aparece dentro de fullText
 function howManyTextInPara(fullText: string, searchTerm: string): number {
@@ -16,7 +16,7 @@ function howManyTextInPara(fullText: string, searchTerm: string): number {
         //  comenzando la búsqueda desde indiceDesde; o -1 si no se encuentra dicho valor.
         let posicion:number= fullText.indexOf(searchTerm);
         while (posicion != -1) { //mientras se encuentre la palabra buscada
-            contador++;           //incremento el contador
+            contador++;           //incremento el contadorS
             //sigo buscando desde la posicion siguiente a la encontrada                                
             posicion = fullText.indexOf(searchTerm, posicion + 1);
         }
